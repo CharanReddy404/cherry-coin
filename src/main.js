2b0c893245a1b6589777b74e1cc5bd2be3510ede
CharanReddy404 <Charanlucky6143@gmail.com>
2024-04-14 07:05:22 +0530

    signing transactions implemented

---
+++ src/main.js
@@ -0,0 +1,31 @@
+const { Blockchain, Transaction } = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+const myKey = ec.keyFromPrivate(
+  'b0aa0946f7498c197e1a55fae51782dd8ec2937d8b64bab246a2edc2516262da'
+);
+const myWalletAddress = myKey.getPublic('hex');
+
+let cherryCoin = new Blockchain();
+
+const tx1 = new Transaction(
+  myWalletAddress,
+  'to address public key gone here',
+  10
+);
+tx1.signTransaction(myKey);
+cherryCoin.addTransaction(tx1);
+
+console.log('\n strating the miner...');
+cherryCoin.minePendingTransactions(myWalletAddress);
+
+console.log(
+  '\n Balance of miner-reward is',
+  cherryCoin.getBalanceOfAddresss(myWalletAddress)
+);
+console.log(JSON.stringify(cherryCoin.chain, null, 4));
+cherryCoin.chain[1].transactions[0].amount = 1;
+console.log(JSON.stringify(cherryCoin.chain, null, 4));
+
+console.log('Is Chain valid?', cherryCoin.isChainValid());
